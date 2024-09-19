63e587070e854399768745f33ca6eea809ccb1c2
Dinesh Kumar <dinesh.dk4934@gmail.com>
2024-09-19 20:09:44 +0530

    Test case modified

--- Basics/Factorial_Number.py
+++ Basics/Factorial_Number.py
@@ -9,14 +9,12 @@ def factorial(n):
 
 # Test case 1
 def test_factorial():
-    # Test case 1: Factorial of 0
+    # Test case 1:
     assert factorial(0) == 1
-    # Test case 2: Factorial of 5
+    # Test case 2:
     assert factorial(5) == 120
-    # Test case 3: Factorial of 1
+    # Test case 3:
     assert factorial(1) == 1
-    # Test case 4: Factorial of 10
-    assert factorial(10) == 3628800
     print("All test cases passed!")
 
 # Running test case
